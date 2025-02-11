7034c7ba8274ce3c3b1d45dd9850a3f20f5eeee1
eltoncox <elton.unix@gmail.com>
2025-02-10 21:18:01 -0300

    Validando valores inválidos com bean Validation.

--- src/main/java/com/elton/algamoney_api/resource/CategoriaResource.java
+++ src/main/java/com/elton/algamoney_api/resource/CategoriaResource.java
@@ -1,6 +1,5 @@
 package com.elton.algamoney_api.resource;
 
-
 import com.elton.algamoney_api.model.Categoria;
 import com.elton.algamoney_api.repository.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 import java.util.Optional;
@@ -16,27 +16,24 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/categorias") // mapeamento para o recurso http://localhost:8080/categorias
 public class CategoriaResource {
-	
-	@Autowired //para implementar a interface de CategoriaRepository
-	private CategoriaRepository categoriaRepository;
-	
 
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	@GetMapping
 	public List<Categoria> listar() {
-		return categoriaRepository.findAll();// exemplo: SQL - select * from categoria
+		return categoriaRepository.findAll();
 	}
-	
+
 	@PostMapping
-	public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response) {
-	  Categoria categoriaSalva = categoriaRepository.save(categoria);
+	public ResponseEntity<Categoria> criar(@Valid @RequestBody Categoria categoria, HttpServletResponse response) {
+		Categoria categoriaSalva = categoriaRepository.save(categoria);
 
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+				.buildAndExpand(categoriaSalva.getCodigo()).toUri();
+		response.setHeader("Location", uri.toASCIIString());
 
-	  URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")// apartir da uri da requisição atual
-	  .buildAndExpand(categoriaSalva.getCodigo()).toUri();	           
-	  response.setHeader("location", uri.toASCIIString()); 
-	  
-	  return ResponseEntity.created(uri).body(categoriaSalva);// Status 201 Created com cabeça
+		return ResponseEntity.created(uri).body(categoriaSalva);
 	}
 	
 	@GetMapping("/{codigo}")
