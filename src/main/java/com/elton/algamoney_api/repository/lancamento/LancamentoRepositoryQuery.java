97d57530855e036d5790e872e8fe8ae85919c068
eltoncox <elton.unix@gmail.com>
2025-02-14 17:06:07 -0300

    paginando com Pageable.

--- src/main/java/com/elton/algamoney_api/repository/lancamento/LancamentoRepositoryQuery.java
+++ src/main/java/com/elton/algamoney_api/repository/lancamento/LancamentoRepositoryQuery.java
@@ -1,13 +1,11 @@
 package com.elton.algamoney_api.repository.lancamento;
 
-
 import com.elton.algamoney_api.model.Lancamento;
 import com.elton.algamoney_api.repository.filter.LancamentoFilter;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
-import java.util.List;
 
 public interface LancamentoRepositoryQuery {
-
-	public List<Lancamento> filtrar(LancamentoFilter lancamentoFilter);
-	
+	public Page<Lancamento> filtrar(LancamentoFilter lancamentoFilter, Pageable pageable);
 }
--- src/main/java/com/elton/algamoney_api/resource/LancamentoResource.java
+++ src/main/java/com/elton/algamoney_api/resource/LancamentoResource.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import com.elton.algamoney_api.event.RecursoCriadoEvent;
-import com.elton.algamoney_api.exceptionhandler.AlgamoneyExceptionHandler;
+import com.elton.algamoney_api.exceptionhandler.AlgamoneyExceptionHandler.Erro;
 import com.elton.algamoney_api.model.Lancamento;
 import com.elton.algamoney_api.repository.LancamentoRepository;
 import com.elton.algamoney_api.repository.filter.LancamentoFilter;
@@ -20,9 +20,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("/lancamentos")
@@ -41,8 +52,8 @@ public class LancamentoResource {
     private MessageSource messageSource;
 
     @GetMapping
-    public List<Lancamento> pesquisar(LancamentoFilter lancamentoFilter) {
-        return lancamentoRepository.filtrar(lancamentoFilter);
+    public Page<Lancamento> pesquisar(LancamentoFilter lancamentoFilter, Pageable pageable) {
+        return lancamentoRepository.filtrar(lancamentoFilter, pageable);
     }
 
     @GetMapping("/{codigo}")
@@ -59,14 +70,10 @@ public class LancamentoResource {
     }
 
     @ExceptionHandler({ PessoaInexistenteOuInativaException.class })
-    public ResponseEntity<Object> handlePessoaInexistenteOuInativaException(
-                                                    PessoaInexistenteOuInativaException ex) {
-
-        String mensagemUsuario = messageSource.getMessage(
-           "pessoa.inexistente-ou-inativa", null, LocaleContextHolder.getLocale());
+    public ResponseEntity<Object> handlePessoaInexistenteOuInativaException(PessoaInexistenteOuInativaException ex) {
+        String mensagemUsuario = messageSource.getMessage("pessoa.inexistente-ou-inativa", null, LocaleContextHolder.getLocale());
         String mensagemDesenvolvedor = ex.toString();
-        List<AlgamoneyExceptionHandler.Erro> erros = Arrays.asList(
-                new AlgamoneyExceptionHandler.Erro(mensagemUsuario, mensagemDesenvolvedor));
+        List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
         return ResponseEntity.badRequest().body(erros);
     }
 
@@ -76,4 +83,3 @@ public class LancamentoResource {
         lancamentoRepository.deleteById(codigo);
     }
 }
-
